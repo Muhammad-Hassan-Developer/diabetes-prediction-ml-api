7ad52074a4640ec5e5a7b1cc9047b466065a0c1f
Muhammad-Hassan-Developer <hassanbscs1@gmail.com>
2025-12-31 03:01:01 +0500

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-uvicorn app:app --host 0.0.0.0 --port ${PORT:-8000}
+# uvicorn app:app --host 0.0.0.0 --port ${PORT:-8000}
+#!/bin/bash
+gunicorn app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:${PORT:-8000}
